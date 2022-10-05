df94e2eac03561159bb6619a7c1389092f098337
gpierobon <gpierobon@users.noreply.github.com>
2022-10-05 17:55:41 +1100

    Restructure and additional checks

--- pre.py
+++ pre.py
@@ -3,22 +3,98 @@ import numpy as np
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
+key      = sys.argv[1]
+ptype    = int(sys.argv[2])  
+fclass   = sys.argv[3]
+fmflr    = sys.argv[4]
+simfol   = sys.argv[5]
 
-ptype  = int(sys.argv[1])  
-fclass = sys.argv[2]
-fmflr  = sys.argv[3]
-simfol = sys.argv[4]
-f_pk   = sys.argv[5]
-f_f    = sys.argv[6] 
+pr_flag  = int(sys.argv[6])
+zi       = int(sys.argv[7])
+zf       = int(sys.argv[8])
 
-if ptype == 1: raise Exception('Not implemented!')
+out_pk   = sys.argv[9]
+out_f    = sys.argv[10]
 
-if ptype == 2:
+#####################################################################################################
+
+def create_mflr_file(ini_file,muflr_file):
+    d = {}
+    with open(f"{ini_file}.ini") as f:
+        for line in f:
+            if "#" in line:
+                continue
+            if line.startswith('root'):
+                break
+            d[line.split()[0]] = line.split()[-1]
+
+    h        = d['h']
+    Omega_m  = d['Omega_m']
+    Omega_b  = d['Omega_b']
+    Omega_nu = d['Omega_ncdm']
+    sigma8   = d['sigma8']
+    n_s      = d['n_s']
+
+    T_cmb_k  = 2.7255
+    w0       = -1
+    wa       = 0
+    cs_1     = 0
+    cs_2     = 0
+    print_r  = pr_flag
+    notu     = 0
+    z_i      = zi
+    nrs      = 1
+    z_f      = zf
+    cambf    = '/srv/scratch/cppcnbody/MuFLR/nu05_transfer_z0.dat'
+    nu_appr  = 1
+
+    fout = open(f'{muflr_file}.txt','w+')
+    fout.write(str(n_s)+" # n_s: scalar spectral index \n")
+    fout.write(str(sigma8)+"\n")
+    fout.write(str(h)+"\n")
+    fout.write(str(Omega_m)+"\n")
+    fout.write(str(Omega_b)+"\n")
+    fout.write(str(Omega_nu)+"\n")
+    fout.write(str(T_cmb_k)+"\n")
+    fout.write(str(w0)+"\n")
+    fout.write(str(wa)+"\n")
+    fout.write(str(cs_1)+"\n")
+    fout.write(str(cs_2)+"\n")
+    fout.write(str(print_r)+"\n")
+    fout.write(str(notu)+"\n")
+    fout.write(str(z_i)+"\n")
+    fout.write(str(nrs)+"\n")
+    fout.write(str(z_f)+"\n")
+    fout.write(str(cambf)+"\n")
+    fout.write(str(nu_appr)+"\n")
+    fout.close()
+
+def scale_back(fclass,fmflr,simfol,f_pk,f_f):
     k,Pk_class = np.loadtxt(f"{fclass}.dat",unpack=True)
-    mf_data    = np.loadtxt(f"{fmflr}.txt")
+    mf_data    = np.loadtxt(f"{fmflr}.txt",skiprows=4)
     Pk_i       = interp1d(k,Pk_class,kind='cubic')
     np.savetxt(str(simfol)+'/'+str(f_pk)+'.txt',np.column_stack((mf_data[:,1],Pk_i(mf_data[:,1])*mf_data[:,2]**2)))
     np.savetxt(str(simfol)+'/'+str(f_f)+'.txt', np.column_stack((mf_data[:,1],mf_data[:,3])))
 
+#def restart_hybrid():
 
-print('\n')
+#def plot_check():
+#    fig,ax = plt.subplots(1,1,figsize=(8,7))
+#    ax.loglog(k,Pk_class,label='raw')
+#    ax.loglog(mf_data[:,1],Pk_i(mf_data[:,1])*mf_data[:,2]**2,label='rescaled')
+#    ax.legend()
+
+#####################################################################################################
+
+if key == 'MuFLR_file':
+    create_mflr_file(fclass,fmflr)
+
+if key == 'pre_sim':
+    if ptype == 0: 
+        scale_back(fclass,fmflr,simfol,out_pk,out_f)
+    if ptype == 1:
+        #restart_hybrid()
+        raise Exception ("Not implemented yet!")
+
+#if key == 'post_sim':
+    # Post processing 
--- pre.sh
+++ pre.sh
@@ -1,54 +1,88 @@
 #!/bin/bash
 
-########################################################################################################
-# Module PreGadget for Nbody simulations of Neutrinos/Axions                                           #
-########################################################################################################
+#######################################################################################################
+ptype=0                                              # 0: Start           (SuperEasy/MFLR/Hybrid)
+                                                     # 1: Restart         (Hybrid only)
+                                                     # 2: Postprocessing  (SuperEasy/MFLR/Hybrid)
 
+# Paths
+SimFolder='./sim'                                    # Path to simulation folder
+Class_Path='/srv/scratch/cppcnbody/class_public'     # Path to class      (Set in our shared folder)
+MuFLR_Path='/srv/scratch/cppcnbody/MuFLR'            # Path to mf module  (Set in our shared folder)
+Hybrid_Path='/srv/scratch/cppcnbody/gadget4-hybrid'  # Path to hybrid     (Set in our shared folder)
 
-ptype=2                                              # 1: SuperEasy
-                                                     # 2: MultiFluid LR
-                                                     # 3: Hybrid      
+# Class file
+ClassFile='nu'                                       # .ini file for class, from folder inifiles
 
+# MuFLR options
+MfIn='params_MuFLR'                                  # Generates MuFLR input file, placed in inifiles
+MfOut='MuFLR_out'                                    # Output of MuFLR result, will go in raw_files
+Opt=0                                                # Print options. 0: D_cb, f, neutrino density  
+zi=200                                               # Initial redshift for MuFLR
+zf=49                                                # Output redshift (gadget4 initial redshift)
 
-ClassPath='/Users/z5278074/gpierobon/class_public'   # Path to class 
-SePath='/Users/z5278074/gpierobon/nu_flr'            # Path to mf module 
-MfPath='/Users/z5278074/gpierobon/nu_mflr'           # Path to mf module 
-SimFolder='./outp'                                 # Path to simulation folder 
+# Opts for starting run
+PkOut='Pk_ini'                                       # Rescaled Pk, .txt file found in SimFolder
+GFOut='GrFact_ini'                                   # Growth factor, .txt file found in SimFolder
 
-ClassStart='ax'                                      # .ini file that runs class
-MfOut='raw_files/mf_out'                                       # Output of mfluid result
-Nk=16                                                # Number of modes for multifluid/supereasy
-zi=49                                                # Initial simulation time
-
-PkOut='Pk_ini'                                       # Create the rescaled Pk,   .txt file
-GFOut='GrFact_ini'                                   # Create the growth factor, .txt file
+# Opts for restarting run
+Stream=0                                             # Stream number
+NumStreams=0                                         # Number of streams 
+# Opts for postprocessing
 
 #######################################################################################################
 
 mkdir -p $SimFolder  
-ClassOut=raw_files/$ClassStart'_pk'              
+ClassOut=raw_files/$ClassFile'_pk'              
 cdir=$(pwd)
-#export $OMP_NUM_THREADS=4
+
 case $ptype in
-    1) 
-    echo 'Prepare SuperEasy simulation' 
-    export PATH=$ClassPath:$PATH; export PATH=$SePath:$PATH 
-    python3 pre.py $ptype $ClassOut $MfOut $SimFolder $file3 $file4
-    ;;    
-    
-    2) 
-    echo 'Prepare MultiFluid simulation'
-    export PATH=$ClassPath:$PATH; export PATH=$MfPath:$PATH
-    echo 'Running Class ...' ;            class          $ClassStart.ini
-    echo 'Running MFLR ...' ;             nu_mflr        $MfOut $Nk $zi
-    echo 'Setting files for Gadget ...' ; python3 pre.py $ptype $ClassOut $MfOut $SimFolder $PkOut $GFOut
-    ;;
-    
-    3) 
-    echo 'Prepare Hybrid simulation'
-     
+
+    0) 
+    echo 'Prepart start of simulation in folder' $SimFolder
     export PATH=$ClassPath:$PATH
     export PATH=$MfPath:$PATH
+
+    echo 'Checking the N_tau and N_mu parameters ...'
+    Ntau1=$(awk -F " |;" '{if ($3 == "N_tau") print $5}' $MuFLR_Path/AU_cosmofunc.h)
+    Nmu1=$(awk -F " |;" '{if ($3 == "N_mu") print $5}'   $MuFLR_Path/AU_cosmofunc.h)
+    Ntau2=$(sed -n '13p' $Hybrid_Path/src/neutrinos/neutrinomflr.h | awk -F " |;" '{print $10}')
+    Nmu2=$(sed -n '14p' $Hybrid_Path/src/neutrinos/neutrinomflr.h | awk -F " |;" '{print $10}')
+    
+    if [[ $Ntau1 != $Ntau2 ]]; 
+    then echo 'MuFLR has N_tau =' $Ntau1 ', while gadget-4 has N_tau =' $Ntau2
+         echo 'Set the parameters equally and recompile!'
+    fi
+    if [[ $Nmu1 != $Nmu2 ]]; 
+    then echo 'MuFLR has N_mu =' $Nmu1 ', while gadget-4 has N_mu =' $Nmu2
+         echo 'Set the parameters equally and recompile!'
+    fi
+    
+    echo 'Running Class in' $Class_Path '...'            
+    $Class_Path/class       inifiles/$ClassFile.ini
+
+    echo 'Compiling MuFLR in' $MuFLR_Path '...'
+    cd $MuFLR_Path; make; cd $cdir
+    
+    echo 'Creating MuFLR input file' $MfIn '...'
+    python3 pre.py MuFLR_file  $ptype inifiles/$ClassFile inifiles/$MfIn $SimFolder $Opt $zi $zf $PkOut $GFOut
+    
+    echo 'Running MuFLR ...'
+    $MuFLR_Path/MuFLR          inifiles/$MfIn.txt > raw_files/$MfOut.txt
+    
+    echo 'Setting files for Gadget ...'
+    python3 pre.py pre_sim     $ptype $ClassOut raw_files/$MfOut $SimFolder $Opt $zi $zf $PkOut $GFOut 
+    echo 'Files' $PkOut 'and' $GFOut 'are ready in' $SimFolder
+    ;;
+    
+    1) 
+    echo 'Restart Hybrid simulation in folder' $SimFolder
+    
+    ;;
+
+    2)
+    echo 'Post-processing simulation from folder' $SimFolder
+
     ;;
 esac
 
